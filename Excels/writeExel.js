bb5a97a20646b8649c9ecc9a0fa37ffae6ecf746
JulianBessone <juliangaspar1997@gmail.com>
2023-12-15 15:31:20 -0300

    feat(MinexusEmployeesVerifExcel) writeExcel function half

--- Excels/writeExel.js
+++ Excels/writeExel.js
@@ -1,5 +1,6 @@
 const XLSX = require('xlsx');
 const fs = require('fs');
+const XlsxPopulate = require('xlsx-populate');
 
 const writeExcel = (data) =>{
     // Crear un conjunto para almacenar nombres de propiedades únicos
@@ -39,7 +40,52 @@ const writeExcel = (data) =>{
 
 }
 
+const writeExcelMinexusEmployees = async (data) => {
+    const workbook = await XlsxPopulate.fromBlankAsync();
+    const worksheet = workbook.sheet('Sheet1');
+
+    // Definir la estructura de la primera fila con los encabezados
+    const headers = ['Nombre', 'Apellido', 'DNI', 'Estado General', 'Motivos Generales', 'Estado Contractual 1', 'Motivos Contractuales 1', 'Estado Contractual 2', 'Motivos Contractuales 2', 'Estado Contractual 3', 'Motivos Contractuales 3'];
+    worksheet.range('A1:G1').value([headers]);
+
+    // Llenar las filas con los datos
+    data.forEach((person, index) => {
+        const row = index + 2; // Empezar desde la fila 2 (fila 1 es para los encabezados)
+
+        worksheet.cell(`A${row}`).value(person.name);
+        worksheet.cell(`B${row}`).value(person.lastName);
+        worksheet.cell(`C${row}`).value(person.dni);
+        if(person.generalReason.length > 0){
+            worksheet.cell(`D${row}`).value('Rechazado')
+            let generalReason = ''
+            person.generalReason.forEach(reason => {
+                const key = Object.keys(reason)[0];
+                generalReason = generalReason + `\n -${key}: ${reason[key]}`
+            })
+            worksheet.cell(`E${row}`).value(generalReason)
+        }
+        if(Object.keys(person.contractsReason).length !== 0){
+            for (const contract in person.contractsReason) {
+                
+            }
+        }
+
+        /*
+        worksheet.cell(`D${row}`).value(person.generalReason[0]['Declaración de Salud Ocupacional']);
+        worksheet.cell(`E${row}`).value(Object.values(person.generalReason[0])[0]);
+        worksheet.cell(`F${row}`).value(person.contractsReason['5400150734'][0]);
+        worksheet.cell(`G${row}`).value(person.contractsReason['5400150734'][1]);
+        */
+    });
+
+    // Guardar el archivo
+    await workbook.toFileAsync('data.xlsx');
+    console.log('Excel generado exitosamente.');
+}
+
+
 module.exports = {
-    writeExcel
+    writeExcel,
+    writeExcelMinexusEmployees
 }
 
--- Minexus/ResponseAnalizer/responseAnalizer.js
+++ Minexus/ResponseAnalizer/responseAnalizer.js
@@ -1,4 +1,49 @@
+const { writeExcelMinexusEmployees } = require("../../Excels/writeExel");
+
 const responseAnalizer = async (response) => {
+    if(response.url() === 'https://eramine.codin.minexus.net/proxy/staff/all_staff') {
+        const responseBody = await response.text();
+        const jsonResponse = JSON.parse(responseBody);
+
+        const employeesNotAllowedList = [] //Lista de empleados rechazados 
+
+        await jsonResponse.entrance.map(e =>{
+            if(e.Estado === 'Rechazado' || e.Estado === 'rechazado'){
+
+                const employee = {} // Por cada empleao rechazado creo un obj temporal que lo agregare a la lista 
+
+                employee.name = e.staff.nombre // nombre 
+                employee.lastName = e.staff.apellido // apellido    
+                employee.dni = e.staff.dni //dni 
+                employee.generalReason = []  // Lista de obj de motivos generales rechazados
+                employee.contractsReason = {}  // Lista de obj de motivos contractuales rechazados
+
+                e.motivo.forEach(obj => {
+                    if(!obj.Contract){
+                        const key = Object.keys(obj)[0];
+                        const decodedKey = JSON.parse(`"${key}"`);
+                        const tempObj = {};
+                        tempObj[decodedKey] = obj[key];
+                        
+                        employee.generalReason.push(tempObj)
+                        return
+                    }
+
+                    const reason = {}
+                    reason[obj.data.name] = obj.data.Status
+                    if(!employee.hasOwnProperty(obj.Contract.sap_id)){ employee.contractsReason[obj.Contract.sap_id] = [] }
+                    employee.contractsReason[obj.Contract.sap_id].push(reason)                 
+                });
+                employeesNotAllowedList.push(employee)
+            }
+        })
+        writeExcelMinexusEmployees(employeesNotAllowedList)
+    }  
+}
+
+
+
+const responseAnalizerClg = async (response) => {
     if(response.url() === 'https://eramine.codin.minexus.net/proxy/staff/all_staff') {
         const responseBody = await response.text();
         const jsonResponse = JSON.parse(responseBody);
